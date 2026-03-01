3a7e63307feaf2bbaeb170de05069ce7a9aac8b1
adityarajput751 <adityarajput751@gmail.com>
2026-03-02 00:48:55 +0530

    done

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB =async()=>{
-    console.log("MONGO URI =", process.env.MONGODB_URI);
     mongoose.connection.on('connected',()=>console.log('dadtabase connected'))
-    await mongoose.connect(`${process.env.MONGODB_URI}/priya-pratap-makeup`)
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
 }
 
 export default connectDB
