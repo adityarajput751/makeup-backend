fbfac5495d67c0214d342053c768b900289ea05f
adityarajput751 <adityarajput751@gmail.com>
2026-03-02 00:52:45 +0530

    ok1

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB =async()=>{
     mongoose.connection.on('connected',()=>console.log('dadtabase connected'))
-    await mongoose.connect(`${process.env.MONGODB_URI}`, {
+    await mongoose.connect(`${process.env.MONGODB_URI}/priya-pratap-makeup`, {
   ssl: true,
   tls: true,
 }).catch((err)=>{
     console.log('database connection error',err)
 })}
-
 export default connectDB
